37601ada0bd1446b5a3f3f9965923c6ff4ae245c
對馬 篤 <>
2022-12-25 21:07:33 +0900

    Prismaインストール

--- src/server.js
+++ src/server.js
@@ -1,15 +1,31 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+let links = [
+    {
+        id: "link-0",
+        description: "GraphQLチュートリアル",
+        url: "www.udemy-graphql.com",
+    },
+];
+
 // スキーマ構造
 const typeDefs = gql`
     type Query {
         info: String!
+        feed: [Link]!
+    }
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `;
 
+// リソルバ関数
 const resolvers = {
     Query: {
         info: () => "HackerNewsクローン",
+        feed: () => links,
     },
 };
 
